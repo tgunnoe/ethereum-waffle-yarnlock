7ecc7a1835107e098f43bf49c7c7003444d6dc36
tgunnoe <t@gvno.net>
2022-03-12 13:05:53 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  outputs = { self, dream2nix }@inputs:
+    let
+      dream2nix = inputs.dream2nix.lib2.init {
+        # modify according to your supported systems
+        systems = [ "x86_64-linux" ];
+        config.projectRoot = ./. ;
+      };
+    in dream2nix.makeFlakeOutputs {
+      source = ./.;
+
+
+    };
+}
